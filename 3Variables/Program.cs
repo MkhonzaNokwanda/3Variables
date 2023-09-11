4360f087ba570e3f8c98123b093b48feb27c81d2
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-11 12:41:25 +0200

    Add project files.

---
+++ 3Variables/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring variables
+            string name;
+            string lastname;
+            int age;
+
+            //initializing variables
+            name = "Kwandenhle";
+            lastname = "Mudau";
+            age = 5;
+
+            //print to screen
+            Console.WriteLine("Hi " + name + " " + lastname);
+            Console.WriteLine("You are " + age);
+
+            //Change the values of the variables
+            name = "Nono";
+            age = 10;
+
+            Console.WriteLine("She never liked her name so she changed it to " + name + " at " + age);
+            //freeze to console
+            Console.ReadLine();
+        }
+    }
+}
